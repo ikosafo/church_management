8cfb4c087a79aa17ae032ab6c775b102fb3aeb0c
Isaac <isaac@getinnotized.com>
2024-02-10 12:27:26 +0000

    Remove meta description

--- includes/authheader.php
+++ includes/authheader.php
@@ -32,9 +32,9 @@ if ($getcount == "0") {
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-  <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-  <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-  <meta name="author" content="PIXINVENT">
+  <meta name="description" content="Login - Church Management System">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
   <title>Login - Church Management System</title>
   <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.html">
   <link rel="shortcut icon" type="image/x-icon" href="<?php echo getLogo(); ?>">
